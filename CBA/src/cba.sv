8e4431179165afc55e19aafd42d6a4e48ec0c601
Karan Diwan <karandiwan46@gmail.com>
2026-09-11 20:16:20 +0530

    Carry Bypass Adder

---
+++ CBA/src/cba.sv
@@ -0,0 +1,68 @@
+`default_nettype none
+
+module cba #(
+    parameter WIDTH = 64
+) (
+    input logic [WIDTH-1:0] a, 
+    input logic [WIDTH-1:0] b,
+    input logic c_in,
+    output logic [WIDTH-1:0] sum,
+    output logic c_out
+);
+
+   wire [(WIDTH/4):0] carry;
+   assign carry[0] = c_in;
+   assign c_out = carry[WIDTH/4];
+
+   genvar i; 
+
+   generate 
+    for (i = 0; i < (WIDTH/4); i++) begin: cbu_block
+        cbu addk (.a(a[4*i+3:4*i]), .b(b[4*i+3:4*i]), .c_in(carry[i]), .sum(sum[4*i+3:4*i]), .c_out(carry[i+1]));
+    end
+   endgenerate
+
+endmodule
+
+module cbu #(
+    parameter WIDTH = 4
+) (
+    input logic [WIDTH-1:0] a, 
+    input logic [WIDTH-1:0] b,
+    input logic c_in,
+    output logic [WIDTH-1:0] sum,
+    output logic c_out
+);
+   
+   wire prop_0, prop_1, prop_2, prop_3, prop_cbu;
+   assign prop_0 = a[0] ^ b[0];
+   assign prop_1 = a[1] ^ b[1];
+   assign prop_2 = a[2] ^ b[2];
+   assign prop_3 = a[3] ^ b[3];
+   assign prop_cbu = (prop_0 & prop_1 & prop_2 & prop_3);
+
+   wire [WIDTH:0] carry;
+   assign carry[0] = c_in;
+   assign c_out = prop_cbu ? c_in : carry[WIDTH];
+
+   genvar i; 
+
+   generate 
+    for (i = 0; i < WIDTH; i++) begin: rca_block
+        fa addk (.a(a[i]), .b(b[i]), .c_in(carry[i]), .sum(sum[i]), .c_out(carry[i+1]));
+    end
+   endgenerate
+
+endmodule
+
+module fa (
+    input logic a,
+    input logic b, 
+    input logic c_in,
+    output logic sum,
+    output logic c_out
+);
+
+  assign {c_out, sum} = a + b + c_in;
+
+endmodule
---
+++ CBA/src/cba_tb.sv
@@ -0,0 +1,60 @@
+`timescale 1ns/1ns
+`include "cba.sv"
+
+module testbench;
+
+   parameter WIDTH = 64;
+
+   logic [WIDTH-1:0] a;
+   logic [WIDTH-1:0] b;
+   logic c_in;
+   logic [WIDTH-1:0] sum;
+   logic c_out;
+
+   cba #(
+    .WIDTH(WIDTH)
+   ) dut (
+    .a(a),
+    .b(b),
+    .c_in(c_in),
+    .sum(sum),
+    .c_out(c_out)
+   );
+
+   initial begin
+
+   $monitor("time = %0t | a = %h | b = %h | c_in = %h | sum = %h | c_out = %h ", $time, a, b, c_in, sum, c_out);
+
+   $dumpfile("Sim.vcd");
+   $dumpvars(0, testbench);
+
+   a = 64'b0;
+   b = 64'b0;
+   c_in = 1'b1;
+   #5;
+
+   a = 64'hF0F0_F0F0_F0F0_F0F0;
+   b = 64'h0F0F_0F0F_0F0F_0F0F;
+   c_in = 1'b0;
+   #5;
+
+   a = 64'hFFFF_FFFF_FFFF_FFFF;
+   b = 64'h0000_0000_0000_0001;
+   c_in = 1'b0;
+   #5;
+
+   a = 64'h0000_0000_0000_00B8;
+   b = 64'h0000_0000_0000_0017;
+   c_in = 1'b1;
+   #5;
+
+   a = 64'hFFFF_FFFF_FFFF_FFFF;
+   b = 64'hFFFF_FFFF_FFFF_FFFF;
+   c_in = 1'b1;
+   #5;
+
+   $display("Simulation Complete!");
+   $finish;
+
+   end
+endmodule
